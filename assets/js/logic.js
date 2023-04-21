9e6eed82b62bf1d4d298c129a45126a44d8f44d2
ewagorka <ewagorka99@gmail.com>
2023-04-21 19:18:25 +0100

    Created JavaScript files

---
+++ assets/js/logic.js
@@ -0,0 +1,6 @@
+var timerEl = document.getElementById('time');
+var startScreenEl = document.getElementById('start');
+var startButton = document.getElementById('start');
+var questionsEl = document.getElementById('questions');
+var endSccreenEl = document.getElementById('end-screen');
+var feedbackEl = document.getElementById('feedback');
