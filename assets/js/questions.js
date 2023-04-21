b0684fb5326f23f2b652a0650ca7447655250097
ewagorka <ewagorka99@gmail.com>
2023-04-21 19:45:26 +0100

    Added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        option1: "1. strings",
+        option2: "2. booleans",
+        option3: "3. alerts",
+        option4: "4. numbers",
+        answer: "alerts"
+
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with ________.",
+        option1: "1. quotes",
+        option2: "2. curly brackets",
+        option3: "3. parenthesis",
+        option4: "4. square brackets",
+        answer: "3. parenthesis"
+
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______.",
+        option1: "1. numbers and strings",
+        option2: "2. other arrays",
+        option3: "3. booleans",
+        option4: "4. all of the above",
+        answer: "4. all of the above"
+
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        option1: "1. commas",
+        option2: "2. curly brackets",
+        option3: "3. quotes",
+        option4: "4. parenthesis",
+        answer: "3. quotes"
+
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        option1: "1. JavaScript",
+        option2: "2. terminal/bash",
+        option3: "3. for loops",
+        option4: "4. console.log",
+        answer: "4. console.log"
+
+    },
+]
